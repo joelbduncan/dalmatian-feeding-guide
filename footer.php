11a4cf429444d9a3ef51818b2f5bc40fbd7f2b6c
Joel Duncan <joel@slethen.net>
2017-01-30 15:53:53 +0000

    Add navbar.php & footer.php

---
+++ footer.php
@@ -0,0 +1,47 @@
+<div class="container">
+  <hr>
+  <footer class="footer">
+    <div class="container">
+      <p class="text-muted">Created by Joel Duncan <a href="https://slethen.io">slethen.io</p>
+    </div>
+  </footer>
+</div>
+
+<script>
+var themes = {
+    "flatly" : "//bootswatch.com/flatly/bootstrap.min.css",
+    "darkly" : "//bootswatch.com/darkly/bootstrap.min.css"
+}
+$(function(){
+    function getCookie(c_name) {
+        if (document.cookie.length > 0) {
+            c_start = document.cookie.indexOf(c_name + "=");
+            if (c_start != -1) {
+                c_start = c_start + c_name.length + 1;
+                c_end = document.cookie.indexOf(";", c_start);
+                if (c_end == -1) c_end = document.cookie.length;
+                return unescape(document.cookie.substring(c_start, c_end));
+            }
+        }
+        return "default";
+    }
+
+    function setCookie(c_name, value) {
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + 7);
+        document.cookie = c_name + "=" + escape(value) + ";path=/;expires=" + exdate.toUTCString();
+    }
+
+    var setTheme = getCookie('setTheme');
+
+    var themesheet = $('<link href="'+themes[getCookie('setTheme')]+'" rel="stylesheet" />');
+
+    themesheet.appendTo('head');
+
+    $('.theme-link').click(function(){
+        var themeurl = themes[$(this).attr('data-theme')];
+        setCookie('setTheme', $(this).attr('data-theme'));
+        themesheet.attr('href',themeurl);
+    });
+});
+</script>
---
+++ navbar.php
@@ -0,0 +1,50 @@
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">Dalmatian Feeding Guide</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li <?php if (basename($_SERVER['PHP_SELF']) == 'index.php') echo 'class="active"' ?>><a href="index.php">Home</a></li>
+        <li <?php if (basename($_SERVER['PHP_SELF']) == 'purine-guide.php') echo 'class="active"' ?>
+        <li <?php if (basename($_SERVER['PHP_SELF']) == 'advanced-purine-guide.php') echo 'class="active"' ?>
+            <?php if (basename($_SERVER['PHP_SELF']) == 'dry-food-guide.php') echo 'class="active"' ?> class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Guides<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li <?php if (basename($_SERVER['PHP_SELF']) == 'purine-guide.php') echo 'class="active"' ?>><a href="purine-guide.php">Purine Table</a></li>
+            <li <?php if (basename($_SERVER['PHP_SELF']) == 'advanced-purine-guide.php') echo 'class="active"' ?>><a href="advanced-purine-guide.php">Advanced Purine Table</a></li>
+            <li <?php if (basename($_SERVER['PHP_SELF']) == 'dry-food-guide.php') echo 'class="active"' ?>><a href="dry-food-guide.php">Dry Food</a></li>
+          </ul>
+        </li>
+        <li <?php if (basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'class="active"' ?> class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li <?php if (basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'class="active"' ?>><a href="contact.php">Contact Us</a></li>
+            <li role="separator" class="divider"></li>
+            <li class="dropdown-header">Useful Links</li>
+            <li><a href="http://www.dalmatianwelfare.co.uk/">Dalmatian Welfare</a></li>
+          </ul>
+        </li>
+        <li><a href="faq.php"><span class="glyphicon glyphicon-book"></span> FAQ</a></li>
+      </ul>
+      <ul class="nav pull-right">
+      <div class="dropdown">
+        <button class="btn btn-default navbar-btn dropdown-toggle pull-right" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+          Theme <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+          <li><a href="#" data-theme="flatly" class="theme-link">Light</a></li>
+          <li><a href="#" data-theme="darkly" class="theme-link">Dark</a></li>
+        </ul>
+      </ul>
+      </div>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
