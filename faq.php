6613981ff679a7c4bf431b8977d05bee1a3c7e13
Joel Duncan <joel@slethen.io>
2017-01-31 16:08:52 +0000

    Start MySQL driven FAQ page

--- faq.php
+++ faq.php
@@ -21,12 +21,58 @@
 <?php include('navbar.php'); ?>
 
 <div class="container">
-  <div class="jumbotron">
-    <h1>FAQ Under Construction</h1>
-    <h2>Sorry about that!</h2>
-    <h1 class="glyphicon glyphicon-wrench"></h1>
+  <!-- Page Header -->
+  <div class="row">
+      <div class="col-lg-12">
+          <h1 class="page-header">FAQ
+                <!--<small>Link to feeding practices</small>-->
+          </h1>
+      </div>
   </div>
 </div>
+
+<?php
+include 'config.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, question, answer FROM faq";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo '<div class="container ">
+          <div class="panel-group" id="faqAccordion">
+              <div class="panel panel-default ">
+                  <div class="panel-heading accordion-toggle question-toggle collapsed" data-toggle="collapse" data-parent="#faqAccordion" data-target="#' . $row["id"] .'">
+                       <h4 class="panel-title">
+                          <a href="#" class="ing">Q: ' . $row["question"] .'</a>
+                    </h4>
+
+                  </div>
+                  <div id="' . $row["id"] .'" class="panel-collapse collapse" style="height: 0px;">
+                      <div class="panel-body">
+                           <h5><span class="label label-primary">Answer</span></h5>
+
+                          <p>' . $row["answer"] .'</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!--/panel-group-->
+      </div>';
+
+    }
+} else {
+    echo "0 results";
+}
+?>
 
 <?php include('footer.php'); ?>
 
