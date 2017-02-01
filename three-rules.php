b4aea07491efb0315659d16e5d8c83311c11244b
Joel Duncan <joel@slethen.io>
2017-02-01 21:56:59 +0000

    Grammer and spelling check

--- three-rules.php
+++ three-rules.php
@@ -24,7 +24,7 @@
   <!-- Page Header -->
   <div class="row">
       <div class="col-lg-12">
-          <h1 class="page-header">Three Rules To a Healthy Dalmatian
+          <h1 class="page-header">Three Rules to a Healthy Dalmatian
           </h1>
       </div>
   </div>
@@ -35,18 +35,18 @@
     <div class="col-md-4">
       <h2><small>One</small></h2>
       <h2>A Healthy Diet Low in Purines</h2>
-      <p>Its a misconception that low purines means low protein. Dalmatian need a relatively high protein diet but due to to their unique uric acid metabolism this must be human-grade protein low in purines. Suitable sources of low purine protein sources can be found in our <a href="purine-guide.php">Purine Guide.</a> Also check out our <a href="dry-food.php">Dry Food Guide.</a></p>
+      <p>Its a misconception that low purines means low protein. Dalmatians need a relatively high protein diet but due to to their unique uric acid metabolism this must be human-grade protein low in purines. Suitable sources of low purine protein sources can be found in our <a href="purine-guide.php">Purine Guide.</a> Also check out our <a href="dry-food.php">Dry Food Guide.</a></p>
 
     </div>
     <div class="col-md-4">
       <h2><small>Two</small></h2>
       <h2>Frequent and Regular Exercise</h2>
-      <p>Dalmatian are large dogs with excellent endurance and stamina. They require a minimum of an hour of exercise, once or preferably twice a day, this should include plenty of off lead running and play. Under exercised Dalmatian may suffer from stress, hyperactivity and in extreme cases aggression.</p>
+      <p>Dalmatians are large dogs with excellent endurance and stamina. They require a minimum of an hour of exercise, once or preferably twice a day, this should include plenty of off lead running and play. Under exercised Dalmatians may suffer from stress, hyperactivity and in extreme cases aggression.</p>
    </div>
     <div class="col-md-4">
       <h2><small>Three</small></h2>
       <h2>Plenty of Fresh Water</h2>
-      <p>Dalmatian should always have fresh clean water to drink and have access to outdoors so that they can regularly urinate.</p>
+      <p>Dalmatians should always have fresh clean water to drink and have access to outdoors so that they can regularly urinate.</p>
     </div>
   </div>
 </div>
