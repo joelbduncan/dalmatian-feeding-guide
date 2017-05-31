dd9369892445477b530c0560025751abe4d4dae2
Joel Duncan <joel@slethen.net>
2017-05-31 17:37:54 +0100

    Add Friends page with Cyprus House Pride & Fergusa

---
+++ friends.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Dalmatian Feeding Guide</title>
+
+<head>
+  <meta charset=utf-8>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css" href="css/friends.css" />
+  <link rel="stylesheet" type="text/css" href="//bootswatch.com/flatly/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="//bootswatch.com/flatly/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/table.js"></script>
+  <script src="js/bootstrap-table.js"></script>
+</head>
+
+<body>
+
+<?php include('navbar.php'); ?>
+
+<div class="container">
+  <!-- Page Header -->
+  <div class="row">
+      <div class="col-lg-12">
+          <h1 class="page-header">Our Friends
+                <!--<small>Link to feeding practices</small>-->
+          </h1>
+      </div>
+  </div>
+  <!-- /.row -->
+
+    <div class="col-sm-6 col-md-6 col-lg-6 mt-4">
+        <div class="card card-inverse card-info">
+            <img class="card-img-top" src="img/cyprus-house-pride-banner.jpg">
+            <div class="card-block">
+                <h4 class="card-title">Cyprus Pride House</h4>
+                <div class="card-text">
+                    We find forever homes for rescued Cyprus cats and dogs.
+                    <a href="http://cypruspridehouse.org/about/">Read our story</a>
+                </div>
+            </div>
+            <div class="card-footer">
+              <a href="https://www.facebook.com/CyprusPrideHouse/" class="btn btn-info btn-sm" role="button"><span class="fa fa-facebook"></a>
+              <a href="https://twitter.com/Cyp_Pride_House/" class="btn btn-info btn-sm" role="button"><span class="fa fa-twitter"></a>
+              <a href="http://cypruspridehouse.org/" class="btn btn-info btn-sm" role="button"><span class="fa fa-globe"></a>
+              <a class="btn btn-success btn-sm" href="http://cypruspridehouse.org/donate/"><i class="fa fa-heart"></i> Donate</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6 mt-4">
+        <div class="card card-inverse card-info">
+            <img class="card-img-top" src="img/fergusa-banner.jpg">
+            <div class="card-block">
+                <h4 class="card-title">Fergusa Dog Food Company</h4>
+                <div class="card-text">
+                    The FERGUSA Dog Food Company is here to provide the discerning dog owner with premium dog food at practical prices.
+                </div>
+            </div>
+            <div class="card-footer">
+              <a href="https://www.facebook.com/Fergusa-Dog-Food-Company-411862845845170/" class="btn btn-info btn-sm" role="button"><span class="fa fa-facebook"></a>
+              <a href="https://twitter.com/FergusaDogFood" class="btn btn-info btn-sm" role="button"><span class="fa fa-twitter"></a>
+              <a href="https://www.fergusadogfoodcompany.co.uk/" class="btn btn-info btn-sm" role="button"><span class="fa fa-globe"></a>
+              <a class="btn btn-success btn-sm" href="https://www.fergusadogfoodcompany.co.uk/shop"><i class="fa fa-shopping-bag"></i> Shop</a>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+<?php include('footer.php'); ?>
+
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -13,13 +13,11 @@
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
         <li <?php if (basename($_SERVER['PHP_SELF']) == 'index.php') echo 'class="active"' ?>><a href="index.php">Home</a></li>
-        <li <?php if (basename($_SERVER['PHP_SELF']) == 'three-rules.php') echo 'class="active"' ?>
         <li <?php if (basename($_SERVER['PHP_SELF']) == 'purine-guide.php') echo 'class="active"' ?>
         <li <?php if (basename($_SERVER['PHP_SELF']) == 'advanced-purine-guide.php') echo 'class="active"' ?>
             <?php if (basename($_SERVER['PHP_SELF']) == 'dry-food-guide.php') echo 'class="active"' ?> class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Guides<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li <?php if (basename($_SERVER['PHP_SELF']) == 'three-rules.php') echo 'class="active"' ?>><a href="three-rules.php">Three Rules</a></li>
             <li <?php if (basename($_SERVER['PHP_SELF']) == 'purine-guide.php') echo 'class="active"' ?>><a href="purine-guide.php">Purine Table</a></li>
             <li <?php if (basename($_SERVER['PHP_SELF']) == 'advanced-purine-guide.php') echo 'class="active"' ?>><a href="advanced-purine-guide.php">Advanced Purine Table</a></li>
             <li <?php if (basename($_SERVER['PHP_SELF']) == 'dry-food-guide.php') echo 'class="active"' ?>><a href="dry-food-guide.php">Dry Food</a></li>
@@ -29,12 +27,13 @@
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li <?php if (basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'class="active"' ?>><a href="contact.php">Contact Us</a></li>
+            <li <?php if (basename($_SERVER['PHP_SELF']) == 'friends.php') echo 'class="active"' ?>><a href="friends.php">Friends</a></li>
             <li role="separator" class="divider"></li>
             <li class="dropdown-header">Useful Links</li>
             <li><a href="http://www.dalmatianwelfare.co.uk/">Dalmatian Welfare</a></li>
           </ul>
         </li>
-        <li <?php if (basename($_SERVER['PHP_SELF']) == 'faq.php') echo 'class="active"' ?>><a href="faq.php"><span class="glyphicon glyphicon-book"></span> FAQ</a></li>
+        <li><a href="faq.php"><span class="glyphicon glyphicon-book"></span> FAQ</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li>
