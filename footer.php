0a8823bf0c86216753ca881d190190df9215a6f4
Joel Duncan <joel@slethen.io>
2019-02-07 16:04:09 +0000

    Made with love Font Awesome include

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
 <div class="container">
   <hr>
   <footer class="footer">
