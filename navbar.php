130f9b2ac601890e555c12f3599bae04cc92a4c6
Joel Duncan <joel@slethen.io>
2020-05-28 23:12:23 +0100

    Remove .php from links, requires Nginx mod

--- navbar.php
+++ navbar.php
@@ -1,23 +1,23 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 <div class="container">
-  <a class="navbar-brand" href="index.php">Dalmatian Feeding Guide</a>
+  <a class="navbar-brand" href="/">Dalmatian Feeding Guide</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item <?php if (basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active' ?>">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown  <?php if (basename($_SERVER['PHP_SELF']) == 'three-rules.php') echo 'active' ?><?php if (basename($_SERVER['PHP_SELF']) == 'purine-guide.php') echo 'active' ?><?php if (basename($_SERVER['PHP_SELF']) == 'advanced-purine-guide.php') echo 'active' ?><?php if (basename($_SERVER['PHP_SELF']) == 'dry-food-guide.php') echo 'active' ?>">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Guides
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'three-rules.php') echo 'active' ?>" href="three-rules.php">Three Rules</a>
-          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'purine-guide.php') echo 'active' ?>" href="purine-guide.php">Purine Table</a>
-          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'advanced-purine-guide.php') echo 'active' ?>" href="advanced-purine-guide.php">Advanced Purine Table</a>
-          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'dry-food-guide.php') echo 'active' ?>" href="dry-food-guide.php">Dry Food</a>
+          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'three-rules.php') echo 'active' ?>" href="three-rules">Three Rules</a>
+          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'purine-guide.php') echo 'active' ?>" href="purine-guide">Purine Table</a>
+          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'advanced-purine-guide.php') echo 'active' ?>" href="advanced-purine-guide">Advanced Purine Table</a>
+          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'dry-food-guide.php') echo 'active' ?>" href="dry-food-guide">Dry Food</a>
         </div>
       </li>
       <li class="nav-item dropdown <?php if (basename($_SERVER['PHP_SELF']) == 'friends.php') echo 'active' ?><?php if (basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'active' ?>">
@@ -25,15 +25,15 @@
           Contact
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'active' ?>" href="contact.php">Contact Us</a>
-          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'friends.php') echo 'active' ?>" href="friends.php">Friends</a>
+          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'active' ?>" href="contact">Contact Us</a>
+          <a class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'friends.php') echo 'active' ?>" href="friends">Friends</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-header">Useful Links</a>
           <a class="dropdown-item" href="http://www.dalmatianwelfare.co.uk/">Dalmatian Welfare</a>
         </div>
       </li>
       <li class="nav-item <?php if (basename($_SERVER['PHP_SELF']) == 'faq.php') echo 'active' ?>">
-        <a class="nav-link" href="faq.php"><i class="fas fa-book"></i> FAQ</a>
+        <a class="nav-link" href="faq"><i class="fas fa-book"></i> FAQ</a>
       </li>
     </ul>
     <div class="dropdown">
