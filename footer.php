640ad81087cb77d721235918a5de29f06e535bac
Joel Duncan <joel@slethen.io>
2022-02-21 15:50:11 +0000

    Add buy me a coffee link

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="joelbduncan" data-description="Support me on Buy me a coffee!" data-message="Hey 👋 you can now support keeping my projects Ad Free by buying me a coffee ☕." data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
 <div class="container">
